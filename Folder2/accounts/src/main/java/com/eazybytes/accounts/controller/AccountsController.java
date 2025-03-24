49a420f36ee450603bd8a638b7538e640dbbb905
nelsonortfan <n.ortizf@uniandes.edu.co>
2025-03-23 21:37:46 -0500

    Agregando Constants, Mappers, Services y Excepciones

--- Folder2/accounts/src/main/java/com/eazybytes/accounts/controller/AccountsController.java
+++ Folder2/accounts/src/main/java/com/eazybytes/accounts/controller/AccountsController.java
@@ -1,15 +1,33 @@
 package com.eazybytes.accounts.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.eazybytes.accounts.constants.AccountsContants;
+import com.eazybytes.accounts.dto.CustomerDto;
+import com.eazybytes.accounts.dto.ResponseDto;
+import com.eazybytes.accounts.service.IAccountsService;
+import lombok.AllArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
 
 @RestController
+@RequestMapping(path = "/api", produces = {MediaType.APPLICATION_JSON_VALUE})
+@AllArgsConstructor
 public class AccountsController {
 
+    private IAccountsService iAccountsService;
 
     @GetMapping("sayHello")
     public  String hello(){
         return "Hola Nelson Ortiz, este es microservicio 1 desde el PC gamer!!!";
+    }
+
+    @PostMapping("/create")
+    public ResponseEntity<ResponseDto> createAccount(@RequestBody CustomerDto customerDto){
+        iAccountsService.createAccount(customerDto);
+        return ResponseEntity.status(HttpStatus.CREATED)
+                .body(new ResponseDto(AccountsContants.STATUS_201, AccountsContants.MESSAGE_201));
     }
 
 }
