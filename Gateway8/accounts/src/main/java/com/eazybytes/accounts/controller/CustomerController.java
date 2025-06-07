7e08b8aa965b6dfef1fcebdca1cec2ae97fbe058
nelsonortfan <n.ortizf@uniandes.edu.co>
2025-06-07 15:20:55 -0500

    Base Spring Cloud Gateway

---
+++ Gateway8/accounts/src/main/java/com/eazybytes/accounts/controller/CustomerController.java
@@ -0,0 +1,31 @@
+package com.eazybytes.accounts.controller;
+
+import com.eazybytes.accounts.dto.CustomerDetailsDto;
+import com.eazybytes.accounts.service.ICustomerService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(path = "/api", produces= {MediaType.APPLICATION_JSON_VALUE} )
+@Validated
+public class CustomerController {
+
+    private final ICustomerService iCustomerService;
+
+    public CustomerController(ICustomerService iCustomerService){
+        this.iCustomerService = iCustomerService;
+    }
+
+    @GetMapping("/fetchCustomerDetails")
+    public ResponseEntity<CustomerDetailsDto> fetchCustomerDetails(@RequestParam String mobileNumber){
+        CustomerDetailsDto customerDetailsDto = iCustomerService.fetchCustomersDetails(mobileNumber);
+        return ResponseEntity.status(HttpStatus.OK).body(customerDetailsDto);
+    }
+
+}
