fbf0b72baca037c14be62194628397d9482d6b2f
nelsonortfan <n.ortizf@uniandes.edu.co>
2025-07-17 22:26:23 -0500

    Primera transmision exitosa en un solo sentido, de Accounts a Message

--- RabbitMQ/message/src/main/java/com/eazybytes/message/functions/MessageFunctions.java
+++ RabbitMQ/message/src/main/java/com/eazybytes/message/functions/MessageFunctions.java
@@ -14,6 +14,31 @@ public class MessageFunctions {
     private static final Logger log = LoggerFactory.getLogger(MessageFunctions.class);
 
     @Bean
+    public Function<AccountsMsgDto,AccountsMsgDto> email() {
+        return accountsMsgDto -> {
+            log.info("Sending email with the details : " +  accountsMsgDto.toString());
+            return accountsMsgDto;
+        };
+    }
+
+    @Bean
+    public Function<AccountsMsgDto,Long> sms() {
+        return accountsMsgDto -> {
+            log.info("Sending sms with the details : " +  accountsMsgDto.toString());
+            return accountsMsgDto.accountNumber();
+        };
+    }
+
+}
+
+
+/*
+@Configuration
+public class MessageFunctions {
+
+    private static final Logger log = LoggerFactory.getLogger(MessageFunctions.class);
+
+    @Bean
     public Function<AccountsMsgDto, AccountsMsgDto> email () {
         return accountsMsgDto ->{
             log.info("Sending email with the details " + accountsMsgDto.toString());
@@ -31,3 +56,5 @@ public class MessageFunctions {
 
 
 }
+
+ */
